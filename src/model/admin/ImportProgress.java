4e1c9342bd56ffb3975aac4725c22ad07d36c983
Dieter Biedermann <biedermann.dieter@powersurf.li>
2017-01-05 15:38:52 +0100

    Progress Thread hinzugefügt

--- src/model/admin/ImportProgress.java
+++ src/model/admin/ImportProgress.java
@@ -1,7 +1,32 @@
 package model.admin;
 
+import controller.admin.ImportController;
+
+import java.util.concurrent.BlockingQueue;
+
 /**
  * Created by dieterbiedermann on 04.01.17.
  */
-public class ImportProgress {
+public class ImportProgress extends Thread {
+
+    private ImportController importController;
+
+    public ImportProgress(ImportController importController) {
+        this.importController = importController;
+    }
+
+    public void run() {
+
+        while (!importController.allRowsProcessed()) {
+            importController.showStatus();
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        importController.showStatus();
+    }
+
 }
