6f0a9206eca97322c19dd037f194c09ac7cb54b2
Dieter Biedermann <biedermann.dieter@powersurf.li>
2017-01-08 17:28:37 +0100

    poi class erstellt

--- src/model/common/Poi.java
+++ src/model/common/Poi.java
@@ -1,4 +1,52 @@
 package model.common;
 
 public class Poi {
+
+    // Instanzvariablen
+    private String id;
+    private String name;
+    private PoiCategory poiCategory;
+    private String longitude;
+    private String latitude;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PoiCategory getPoiCategory() {
+        return poiCategory;
+    }
+
+    public void setPoiCategory(PoiCategory poiCategory) {
+        this.poiCategory = poiCategory;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
 }
