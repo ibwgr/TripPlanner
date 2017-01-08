18e2181c55643f048011b1fd0d7873608203a7fb
Dieter Biedermann <biedermann.dieter@powersurf.li>
2017-01-08 12:47:41 +0100

    generelle Fehleranzeige im GUI Framework eingebaut

---
+++ src/view/travel/TripView.java
@@ -0,0 +1,17 @@
+package view.travel;
+
+import javax.swing.*;
+
+public class TripView extends JPanel {
+
+    private JLabel testLabel;
+
+    public TripView() {
+
+        testLabel = new JLabel("test trip view");
+
+        this.add(testLabel);
+
+    }
+
+}
