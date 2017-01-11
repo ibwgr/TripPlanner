7be1d0a2052834083b39c8fb9371b0a1bf19489e
Reto Kaufmann <reto.kaufmann@adon.li>
2017-01-11 21:42:55 +0100

    JTable Version 2, noch nicht feritg

--- src/view/travel/TripView.java
+++ src/view/travel/TripView.java
@@ -1,9 +1,12 @@
 package view.travel;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.border.Border;
+import javax.swing.table.*;
 
+import controller.common.LoginController;
 import controller.common.MainController;
+import controller.travel.TripController;
 import model.travel.Trip;
 
 import java.awt.*;
@@ -13,36 +16,73 @@ public class TripView extends JPanel {
 
     private JLabel testLabel;
 
-    //JTextField userField;
-    //JPasswordField passwordField;
-    //JButton loginButton;
+    /*
+    TODO
+    Fehlend: Headers noch nicht schoen, Groesse passt noch nicht, Soll nicht editierbar sein
+    Fehlend: Details Button mit entspr. Row
+     */
 
-    //LoginController loginController;   ... muss ein TripController sein!
+    TripController tripController;
     MainController mainController;
 
     public TripView(MainController mainController) {
 
-        DefaultTableModel model = new DefaultTableModel();
-        JTable table = new JTable(model);
-        table.setAutoCreateColumnsFromModel(true);
-        table.setPreferredSize(new Dimension(400,200));
+        this.mainController = mainController;
+        tripController = new TripController(this, mainController);
+
+        DefaultTableModel tableModel = new DefaultTableModel();
+        JTable table = new JTable(tableModel);
+        //table.setAutoCreateColumnsFromModel(true);
+        //table.setPreferredSize(new Dimension(400,200));
         table.setAutoCreateRowSorter(true);
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 
-        // Create a couple of columns
-        model.addColumn("Nr");
-        model.addColumn("Trip Name");
-        model.addColumn("From");
-        model.addColumn("To");
-        model.addColumn("Activities");
-
+        // Create columns
+        String[] columnNames = {"Nr", "Trip Name", "From-Date", "To-Date", "Act."};
+        for (String column : columnNames){
+            tableModel.addColumn(column);
+        }
         // TripListe (from Model)
         ArrayList<Trip> tripList = Trip.searchByUser(mainController.getUser());
         for (Trip trip : tripList) {
             // Append a row
-            model.addRow(new Object[]{trip.getId(), trip.getName(),trip.getMinDate(),trip.getMaxDate(), trip.getCountActivities()});
+            tableModel.addRow(new Object[]{trip.getId(), trip.getName(),trip.getMinDate(),trip.getMaxDate(), trip.getCountActivities()});
         }
 
-        this.add(table);
+        resizeColumnWidth(table);
+
+        // Tabelle platzieren
+        JPanel centerPanel = new JPanel(new BorderLayout());
+        centerPanel.add(new JScrollPane( table ), BorderLayout.CENTER);
+
+        // Detailbutton platzieren
+        JPanel southPanel = new JPanel(new BorderLayout());
+        JButton detailButton = new JButton("Detail");
+        detailButton.setActionCommand("detail");
+        detailButton.addActionListener(tripController);
+        southPanel.add(detailButton, BorderLayout.SOUTH);
+
+
+        // alles aufs Hauptpanel platzieren
+        this.add(centerPanel);
+        this.add(southPanel);
+    }
+
+
+    //https://stackoverflow.com/questions/17627431/auto-resizing-the-jtable-column-widths
+    public void resizeColumnWidth(JTable table) {
+        final TableColumnModel columnModel = table.getColumnModel();
+        for (int column = 0; column < table.getColumnCount(); column++) {
+            int width = 20; // Min width
+            for (int row = 0; row < table.getRowCount(); row++) {
+                TableCellRenderer renderer = table.getCellRenderer(row, column);
+                Component comp = table.prepareRenderer(renderer, row, column);
+                width = Math.max(comp.getPreferredSize().width +1 , width);
+            }
+            if(width > 150)
+                width=150;
+            columnModel.getColumn(column).setPreferredWidth(width);
+        }
     }
 
 }
