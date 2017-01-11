5181de95b30676118e0ea104a81f483eafdbcf80
Dieter Biedermann <biedermann.dieter@powersurf.li>
2017-01-11 23:36:32 +0100

    Close Button verbessert

--- src/view/travel/CitySearchView.java
+++ src/view/travel/CitySearchView.java
@@ -29,22 +29,42 @@ public class CitySearchView extends JPanel {
         this.setLayout(new BorderLayout());
 
         // NORTH: Search criteria
+/*
         searchText = new JTextField(10);
-        JButton button = new JButton("Search");
-        button.setActionCommand("searchCity");
-        button.addActionListener(citySearchController);
+        JButton searchButton = new JButton("Search");
+        searchButton.setActionCommand("searchCity");
+        searchButton.addActionListener(citySearchController);
         JPanel flowPanel1 = new JPanel(new FlowLayout());
-//        flowPanel = new GridPanel(1,1,100,20);
-        flowPanel1.add(new JLabel("City name:"));
-        flowPanel1.add(searchText);
-        flowPanel1.add(button);
-        this.add(flowPanel1, BorderLayout.NORTH);
+*/
+        GridPanel gridPanel1 = new GridPanel(1,1,100,20);
+        gridPanel1.addComponentToPanel(searchText = new JTextField(10));
+        gridPanel1.addComponentToPanel(gridPanel1.createButton("Search", "searchCity", citySearchController));
+        gridPanel1.addPanelWithLabel("City name:", true);
+        this.add(gridPanel1, BorderLayout.NORTH);
 
         // CENTER: Result
         this.add(new JScrollPane(searchResult = new JList<>()), BorderLayout.CENTER);
 
         // SOUTH: add Activity, forward to Poi Search
+        GridPanel gridPanel2 = new GridPanel(4,1,300,20);
+/*
+        JPanel flowPanel2 = new JPanel(new FlowLayout());
+        JButton openPoiButton = new JButton("Search POI");
+        flowPanel2.add(openPoiButton);
+        this.add(flowPanel2, BorderLayout.SOUTH);
+*/
 
+        gridPanel2.addComponentToPanel(gridPanel2.createButton("Search POI", "searchPoi", citySearchController));
+        gridPanel2.addPanelWithLabel("Add city to the activity or search for POI:", true);
+
+/*
+        gridPanel2.addComponentToPanel(new JTextField(10));
+        gridPanel2.addPanelWithLabel("Date:", true);
+        gridPanel2.addComponentToPanel(new JTextField(10));
+        gridPanel2.addPanelWithLabel("Comment:", true);
+*/
+
+        this.add(gridPanel2, BorderLayout.SOUTH);
 
         /*
         addComponentToPanel(searchText = new JTextField(10));
