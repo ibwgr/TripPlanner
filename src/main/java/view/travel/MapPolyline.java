e1362f637c5733aa3a0e0ef404b17b0db2b4a700
Reto Kaufmann <reto.kaufmann@adon.li>
2017-01-18 20:46:30 +0100

    Polyline anstatt Polygon

--- src/main/java/view/travel/MapPolyline.java
+++ src/main/java/view/travel/MapPolyline.java
@@ -44,6 +44,7 @@ public class MapPolyline extends MapView {
                     // Setting initial zoom value
                     map.setZoom(5.0);
 
+                    // Creating a path (array of coordinates) that represents a polyline
                     ArrayList<Activity> activityList = Activity.searchByTrip(mainController.getTrip());
                     LatLng[] path = new LatLng[activityList.size()];
                     int i = -1;
@@ -52,11 +53,11 @@ public class MapPolyline extends MapView {
                         path[++i] = latLng;
                         map.setCenter(latLng);
                     }
-                    // Creating a path (array of coordinates) that represents a polyline
-//                    LatLng[] path = {new LatLng(37.772, -122.214),
-//                            new LatLng(21.291, -157.821),
-//                            new LatLng(-18.142, 178.431),
-//                            new LatLng(-27.467, 153.027)};
+                    //Beispiel
+                    //                    LatLng[] path = {new LatLng(37.772, -122.214),
+                    //                            new LatLng(21.291, -157.821),
+                    //                            new LatLng(-18.142, 178.431),
+                    //                            new LatLng(-27.467, 153.027)};
 
                     // Creating a new polyline object
                     Polyline polyline = new Polyline(map);
