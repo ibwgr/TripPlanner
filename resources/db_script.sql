4b4d460d7cb2b48d71ae78ec0b6a4330c0507263
Reto Kaufmann <reto.kaufmann@adon.li>
2017-01-21 21:48:57 +0100

    neue, bessere testdaten

--- resources/db_script.sql
+++ resources/db_script.sql
@@ -245,73 +245,25 @@ from tp_user
 -----------------------------------------------------------
 -- some dummy TRIP data
 -----------------------------------------------------------
-
-INSERT INTO tp_trip (user_id, name)
-VALUES (1, 'Sommerferien Schweden 2016');
-
-INSERT INTO tp_trip (user_id, name)
-VALUES (1, 'Frühlingsferien USA 2017 mit Kunzes aus Bünzen');
-
-INSERT INTO tp_trip (user_id, name)
-VALUES (1, 'Noch völlig unklarer Trip über Ostern');
-
+INSERT INTO tp_trip (user_id, name) VALUES (1, 'Sommerferien Schweden 2016');
+INSERT INTO tp_trip (user_id, name) VALUES (1, 'Frühlingsferien USA 2017 mit Kunzes aus Bünzen');
 COMMIT;
 /*
 select *
 from tp_trip;
 */
 -----------------------------------------------------------
--- some dummy TR_ACTIVITY data
+-- some dummy TR_ACTIVITY data --> load sweden-latest.csv first!
 -----------------------------------------------------------
-
-INSERT INTO tp_activity (trip_id,
-                         poi_id,
-                         date,
-                         comment)
-VALUES (1,
-        (  SELECT id
-           FROM poi
-           ORDER BY id
-           LIMIT 1),
-        to_date ('01.07.2016', 'dd.mm.yyyy'),
-        'direkt nach dem Frühstück');
-
-INSERT INTO tp_activity (trip_id,
-                         poi_id,
-                         date,
-                         comment)
-VALUES (1,
-        (  SELECT id
-           FROM poi
-           ORDER BY longitude
-           LIMIT 1),
-        to_date ('01.07.2016', 'dd.mm.yyyy'),
-        'direkt danach');
-
-INSERT INTO tp_activity (trip_id,
-                         poi_id,
-                         date,
-                         comment)
-VALUES (1,
-        (  SELECT id
-           FROM poi
-           ORDER BY latitude
-           LIMIT 1),
-        to_date ('02.07.2016', 'dd.mm.yyyy'),
-        'je nach Wetter');
-
-INSERT INTO tp_activity (trip_id,
-                         poi_id,
-                         date,
-                         comment)
-VALUES (2,
-        (  SELECT id
-           FROM poi
-           ORDER BY name
-           LIMIT 1),
-        to_date ('08.08.2017', 'dd.mm.yyyy'),
-        'da gibts das beste Bier!');
-
+INSERT INTO tp_activity (trip_id, poi_id, date, comment, city) VALUES ( (select id from tp_trip where name = 'Sommerferien Schweden 2016'), 'N444868954',  to_date('2017-01-01','yyyy-mm-dd'), 'Malmö Flughafen', 'Malmö');
+INSERT INTO tp_activity (trip_id, poi_id, date, comment, city) VALUES ( (select id from tp_trip where name = 'Sommerferien Schweden 2016'), 'N1680808493', to_date('2017-01-02','yyyy-mm-dd'), 'Kommissar Wallander Rundfahrt', 'Ystad');
+INSERT INTO tp_activity (trip_id, poi_id, date, comment, city) VALUES ( (select id from tp_trip where name = 'Sommerferien Schweden 2016'), 'N27377535',   to_date('2017-01-03','yyyy-mm-dd'), 'nur Zwischenstop bei Onkel Björn', 'Kristianstad');
+INSERT INTO tp_activity (trip_id, poi_id, date, comment, city) VALUES ( (select id from tp_trip where name = 'Sommerferien Schweden 2016'), 'W438952439',  to_date('2017-01-04','yyyy-mm-dd'), 'schöner Park', 'Karlshamn');
+INSERT INTO tp_activity (trip_id, poi_id, date, comment, city) VALUES ( (select id from tp_trip where name = 'Sommerferien Schweden 2016'), 'N1599473536', to_date('2017-01-05','yyyy-mm-dd'), 'Schärengarten Rundfahrt mit dem Schiff', 'Karlskrona');
+INSERT INTO tp_activity (trip_id, poi_id, date, comment, city) VALUES ( (select id from tp_trip where name = 'Sommerferien Schweden 2016'), 'W244384754',  to_date('2017-01-06','yyyy-mm-dd'), 'Der kleine Hafen ist grossartig', 'Kristianopel');
+INSERT INTO tp_activity (trip_id, poi_id, date, comment, city) VALUES ( (select id from tp_trip where name = 'Sommerferien Schweden 2016'), 'W36194480',   to_date('2017-01-07','yyyy-mm-dd'), 'Schöner Park', 'Kalmar');
+INSERT INTO tp_activity (trip_id, poi_id, date, comment, city) VALUES ( (select id from tp_trip where name = 'Sommerferien Schweden 2016'), 'N4389197835', to_date('2017-01-08','yyyy-mm-dd'), 'Übernachtung auf Öland', 'Färjestaden');
+COMMIT;
 /*
 select *
 from tp_activity
