387b7581525b517f4a274a82ee2206175f72e822
Dieter Biedermann <biedermann.dieter@powersurf.li>
2017-01-25 23:09:27 +0100

    Mac Screenshots hinzugefügt

--- src/test/java/model/common/ViewInfoTest.java
+++ src/test/java/model/common/ViewInfoTest.java
@@ -1,10 +1,56 @@
 package model.common;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+import testFramework.UnitTest;
+
+import java.awt.*;
 
 /**
  * Created by dieterbiedermann on 25.01.17.
  */
 public class ViewInfoTest {
+
+    @Category({ UnitTest.class })
+    @Test
+    public void getTitleReturnsCorrectTitle() {
+        Component compo = Mockito.mock(Component.class);
+
+        ViewInfo viewInfo = new ViewInfo("title", compo, false, false);
+        String result = viewInfo.getTitle();
+        Assert.assertEquals(result, "title");
+    }
+
+    @Category({ UnitTest.class })
+    @Test
+    public void getCompoReturnsCorrectCompo() {
+        Component compo = Mockito.mock(Component.class);
+
+        ViewInfo viewInfo = new ViewInfo("title", compo, false, false);
+        Component result = viewInfo.getCompo();
+        Assert.assertEquals(result, compo);
+    }
+
+    @Category({ UnitTest.class })
+    @Test
+    public void getShowSubTitleReturnsCorrectShowSubTitle() {
+        Component compo = Mockito.mock(Component.class);
+
+        ViewInfo viewInfo = new ViewInfo("title", compo, false, false);
+        Boolean result = viewInfo.getShowSubTitle();
+        Assert.assertEquals(result, false);
+    }
+
+    @Category({ UnitTest.class })
+    @Test
+    public void getNeedsRefreshReturnsCorrectNeedsRefresh() {
+        Component compo = Mockito.mock(Component.class);
+
+        ViewInfo viewInfo = new ViewInfo("title", compo, false, false);
+        Boolean result = viewInfo.getNeedsRefresh();
+        Assert.assertEquals(result, false);
+    }
 
 }
